6e64625d31f1207fd513c9dad1634cd635acc2c0
Anton Smirnov <werewolfsa@hotmail.com>
2024-08-22 17:31:24 +1000

    Entering variables.

--- devCPlusChapter_2_3_SuperRacing/devCPlusChapter_2_3_SuperRacing.cpp
+++ devCPlusChapter_2_3_SuperRacing/devCPlusChapter_2_3_SuperRacing.cpp
@@ -5,21 +5,28 @@
 
 int main()
 {
+    int mainRaceCircle = 4;
+    int mainInfoCarSpeed = 358;
+    int equipmentEnginePower = 254;
+    int equipmentCarWheels = 93;
+    int equipmentCarWheel = 49;
+    int influenceWeatherWild = 21;
+    int influenceWeatherRain = 17;
     setlocale(LC_ALL,"Russian");
     std::cout << "================== =\n";
-    std::cout << "Супер гонки.Круг 4\n";
+    std::cout << "Супер гонки.Круг: " << mainRaceCircle << "\n";
     std::cout << "================== =\n";
-    std::cout << "Шумахер(358)\n";
+    std::cout << "Шумахер(" << mainInfoCarSpeed << ")\n";
     std::cout << "================== =\n";
     std::cout << "Водитель: Шумахер\n";
-    std::cout << "Скорость : 358\n";
+    std::cout << "Скорость: " << mainInfoCarSpeed << "\n";
     std::cout << "------------------ -\n";
     std::cout << "Оснащение\n";
-    std::cout << "Двигатель : +254\n";
-    std::cout << "Колеса : +93\n";
-    std::cout << "Руль : +49\n";
+    std::cout << "Двигатель : +" << equipmentEnginePower << "\n";
+    std::cout << "Колеса : +" << equipmentCarWheels << "\n";
+    std::cout << "Руль : +" << equipmentCarWheel << "\n";
     std::cout << "------------------ -\n";
     std::cout << "Действия плохой погоды\n";
-    std::cout << "Ветер : -21\n";
-    std::cout << "Дождь : -17\n";
+    std::cout << "Ветер : -" << influenceWeatherWild << "\n";
+    std::cout << "Дождь : -" << influenceWeatherRain << "\n";
 }
