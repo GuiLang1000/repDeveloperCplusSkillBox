a922492b2c657a2c55ebef4bf12a1dac9a86e4bf
GuiLang1000 <anton.smirnove@gmail.com>
2024-08-24 16:55:19 +1000

    Full Code "devProductCostCalculator_chapter_2_5".

---
+++ devProductCostCalculator_chapter_2_5/devProductCostCalculator_chapter_2_5.cpp
@@ -0,0 +1,27 @@
+﻿// devProductCostCalculator_chapter_2_5.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+// Калькулятор для расчёта полной стоимости товара
+
+#include <iostream>
+
+int main()
+{
+    int costProductWiFiRouter = 1500;
+    int costDeliveryProduct = 500;
+    int sailProduct = costProductWiFiRouter * 1 / 10;
+    int fullCostProduct = costProductWiFiRouter + costDeliveryProduct - sailProduct;
+    //...
+    setlocale(LC_ALL, "Russian");
+    std::cout << "||============================================================================================================||\n";
+    std::cout << "|| " << "Калькулятор для расчёта полной стоимости товара с учётом текущих скидок и доставки до вашего района.\n";
+    std::cout << "|| " << "\n";
+    std::cout << "|| " << "Информация о товаре:\n";
+    std::cout << "|| " << "Выберите Товар: " << "WiFi Роутер\n";
+    std::cout << "|| " << "Стоимость товара (р.): " << costProductWiFiRouter << "\n";
+    std::cout << "|| " << "Текущая скидка на данный товар (р.): " << sailProduct << "\n";
+    std::cout << "|| " << "\n";
+    std::cout << "|| " << "Выберите магазин: " << "WOLF Technic. " << "г.Нунайск, ул.Питейная д.1" << "\n";
+    std::cout << "|| " << "Стоимость доставки в ваш район (р.): " << costDeliveryProduct << "\n";
+    std::cout << "|| " << "\n";
+    std::cout << "|| " << "Полная стоимость товара с учётом доставки и текущих скидок равна (р.): " << fullCostProduct << "\n";
+    std::cout << "||============================================================================================================||\n";
+}
