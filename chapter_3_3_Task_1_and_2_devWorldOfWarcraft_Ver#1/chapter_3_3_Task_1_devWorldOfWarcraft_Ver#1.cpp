dbdddd8e140a0cfb1b04cb89cca68561af1319c8
GuiLang1000 <anton.smirnove@gmail.com>
2024-08-25 15:54:16 +1000

    Final fixation program "chapter_3_3_Task_1_and_2_devWorldOfWarcraft_Ver#1", but problem with russian output string after keyboard input wasnot solved.

--- chapter_3_3_Task_1_and_2_devWorldOfWarcraft_Ver#1/chapter_3_3_Task_1_devWorldOfWarcraft_Ver#1.cpp
+++ chapter_3_3_Task_1_and_2_devWorldOfWarcraft_Ver#1/chapter_3_3_Task_1_devWorldOfWarcraft_Ver#1.cpp
@@ -1,4 +1,5 @@
-﻿// Создание Игрового Эккаунта в "World Of Warcraft" -> blizzard.com
+﻿// Task#1 Создание Игрового Эккаунта в "World Of Warcraft" -> blizzard.com
+// Task#2 Программа, которая требует ввести логин и пароль, а после выводит сообщение об успешной регистрации.
 //
 
 #include <iostream>
@@ -7,13 +8,16 @@ int main()
 {
     setlocale(LC_ALL, "Russian");
     
+    //Task#1 and 2
     //...
     std::string answerYesNo;
+    std::string userAccountName;
+    std::string userPassword;
     std::string userName;
     std::string userRaceOrcHuman;
-    std::string userAccountName;
+    std::string userCharacterName;
     std::string characterSpeech = "<<Моя Жизнь принадлежит Орде!!!>>";
-    
+    //...
     //...
     std::cout << "||============================================================================================================||\n";
     std::cout << "||======                                  !!! World Of Warcraft !!!                                     ======||\n";
@@ -23,15 +27,23 @@ int main()
     std::cout << "|| " << "Создать Account? (Введите Да/Нет): ";
     std::cin >> answerYesNo;
     std::cout << "|| " << "\n";
+    std::cout << "|| " << "Придумайте login учётной записи: ";
+    std::cin >> userAccountName;
+    std::cout << "|| " << "Придумайте Password: ";
+    std::cin >> userPassword;
+    std::cout << "|| " << "\n";
+    std::cout << "|| " << "Вы успешно Зарегистрировались " << userAccountName << "!\n";
+    std::cout << "|| " << "\n";
+    std::cout << "|| " << "Ввод игровых данных:\n";
     std::cout << "|| " << "Введите Ваше Имя: ";
     std::cin >> userName;
     std::cout << "|| " << "\n";
     std::cout << "|| " << "Доброго Времени Суток в Перекрёстке Миров, Попаданец " << userName << "!\n";
     std::cout << "|| " << "Выберите Расу Вашего персонажа (Введите Орк/Человек): ";
     std::cin >> userRaceOrcHuman;
-    std::cout << "|| " << "\n";
+    //std::cout << "|| " << "\n";
     std::cout << "|| " << "Введите Имя Вашего Персонажа: ";
-    std::cin >> userAccountName;
+    std::cin >> userCharacterName;
     std::cout << "|| " << "\n";
     std::cout << "|| " << userName << ", Вы подтверждаете введённые данные? (Введите Да/Нет): ";
     std::cin >> answerYesNo;
@@ -40,7 +52,7 @@ int main()
     std::cout << "||_______________________________________________________________\n";
     std::cout << "|| " << "Имя Игрока: " << userName << ".\n";
     std::cout << "|| " << "Раса Игрового Персонажа: " << userRaceOrcHuman << ".\n";
-    std::cout << "|| " << "Имя игрового персонажа: " << userAccountName << ".\n";
+    std::cout << "|| " << "Имя игрового персонажа: " << userCharacterName << ".\n";
     std::cout << "||_______________________________________________________________\n";
     std::cout << "|| " << "\n";
     std::cout << "|| " << "Перемещаемся в Новый Мир ................" << "\n";
@@ -52,4 +64,6 @@ int main()
     std::cout << "|| " << "\n";
     std::cout << "|| " << "ЗАГРУЗКА................\n";
     std::cout << "||============================================================================================================||\n";
-}
+
+    //...
+    }
